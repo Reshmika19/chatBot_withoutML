cb0ae5fce1f8bade4605ca4bb94bda21f1238029
Reshmika19 <113816308+Reshmika19@users.noreply.github.com>
2023-07-06 13:30:09 +0530

    Add files via upload

---
+++ chat.py
@@ -0,0 +1,22 @@
+def chat():
+
+    users=['hai','how are you','I need help','what is your name','good morning','good afternoon','What are you doing','thank you']
+
+    bot=['hai!','I am fine ! what about you','I am here to assist you','my name is ReshBot','good morning','good afternoon','Im an ai model, here to assist you','welcome']
+
+    user_input=input("You : ")
+
+    i=0
+
+    for user in users :
+
+        if user_input==user:
+
+            print("Bot : " +bot[i]) 
+            
+            return chat()
+
+        i=i+1
+
+
+chat()
