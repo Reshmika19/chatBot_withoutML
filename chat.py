6387a5b88ca4434f031c32da7020504a6e121267
Reshmika19 <113816308+Reshmika19@users.noreply.github.com>
2023-07-06 13:38:53 +0530

    Chat.py
    Simple ChatBot using Python Without Machine Learning.
    ChatBot for Python Beginners

--- chat.py
+++ chat.py
@@ -1,8 +1,13 @@
+#this is simple chatbot with machine learning
+
+#chatbot using python for beginners
+
+
 def chat():
 
     users=['hai','how are you','I need help','what is your name','good morning','good afternoon','What are you doing','thank you']
 
-    bot=['hai!','I am fine ! what about you','I am here to assist you','my name is ReshBot','good morning','good afternoon','Im an ai model, here to assist you','welcome']
+    bot=['hai!','I am fine ! what about you','I am here to assist you','my name is chatBot','good morning','good afternoon','Im an ai model, here to assist you','welcome']
 
     user_input=input("You : ")
 
